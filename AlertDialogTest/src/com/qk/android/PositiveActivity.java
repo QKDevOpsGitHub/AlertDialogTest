48f10498cf6c1f470c81dcc45d9bf03c519f571e
Q2659 <Q2659@QKNBK386.Corp.qualitykiosk.com>
2017-06-27 12:26:51 +0530

    V1.0

---
+++ AlertDialogTest/src/com/qk/android/PositiveActivity.java
@@ -0,0 +1,16 @@
+package com.qk.android;
+
+import com.javacodegeeks.android.alertdialogtest.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PositiveActivity extends Activity {
+	@Override
+	   protected void onCreate(Bundle savedInstanceState) {
+	      super.onCreate(savedInstanceState);
+	      setContentView(R.layout.positive_view);
+	   }
+
+}
+
